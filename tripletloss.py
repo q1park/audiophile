2f586e5f6dbaaa89eb939c6cacca42c7efe19eeb
q1park <q1kollab@gmail.com>
2019-05-12 13:38:30 -0400

    Add files via upload

--- tripletloss.py
+++ tripletloss.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
-def _pairwise_distances(embeddings, squared=False):
+def _pairwise_dist(embeddings, squared=False):
     dot_product = tf.matmul(embeddings, tf.transpose(embeddings))
 
     square_norm = tf.diag_part(dot_product)
@@ -15,65 +15,65 @@ def _pairwise_distances(embeddings, squared=False):
         distances = distances * (1.0 - mask)
     return distances
 
-def _anchorpositivemask(labels):
+def _anchorpos_mask(labels):
     labels = labels.numpy()
-    maskAP = labels[:,None] == labels[None:]
-    maskAP[np.eye(labels.size,dtype=bool)] = 0
-    return tf.convert_to_tensor(maskAP, dtype=tf.float32)
+    mask_anchorpos = labels[:,None] == labels[None:]
+    mask_anchorpos[np.eye(labels.size,dtype=bool)] = 0
+    return tf.convert_to_tensor(mask_anchorpos, dtype=tf.float32)
     
-def _anchornegativemask(labels):
+def _anchorneg_mask(labels):
     labels = labels.numpy()
-    maskAN = labels[:,None] != labels[None:]
-    return tf.convert_to_tensor(maskAN, dtype=tf.float32)
+    mask_anchorneg = labels[:,None] != labels[None:]
+    return tf.convert_to_tensor(mask_anchorneg, dtype=tf.float32)
 
 def _tripletmask(labels):
     labels = labels.numpy()
-    maskAP = labels[:,None,None] == labels[None,:,None]
-    maskAN = labels[:,None,None] != labels[None,None,:]
-    maskAPN = np.logical_and(maskAP, maskAN)
+    mask_anchorpos = labels[:,None,None] == labels[None,:,None]
+    mask_anchorneg = labels[:,None,None] != labels[None,None,:]
+    mask_anchorposneg = np.logical_and(mask_anchorpos, mask_anchorneg)
     
-    maskAPN.transpose(0,2,1)[np.eye(labels.size,dtype=bool), :] = 0
-    maskAPN[np.eye(labels.size,dtype=bool), :] = 0
-    maskAPN[:, np.eye(labels.size,dtype=bool)] = 0
-    return tf.convert_to_tensor(maskAPN, dtype=tf.float32)
+    mask_anchorposneg.transpose(0,2,1)[np.eye(labels.size,dtype=bool), :] = 0
+    mask_anchorposneg[np.eye(labels.size,dtype=bool), :] = 0
+    mask_anchorposneg[:, np.eye(labels.size,dtype=bool)] = 0
+    return tf.convert_to_tensor(mask_anchorposneg, dtype=tf.float32)
 
 def batch_all(embeddings, labels, alpha = 0.2, squared=False):
-    pairwise_dist = _pairwise_distances(embeddings, squared=squared)
+    pairwise_dist = _pairwise_dist(embeddings, squared=squared)
 
-    anchor_positive_dist = tf.expand_dims(pairwise_dist, 2)
-    assert anchor_positive_dist.shape[2] == 1, "{}".format(anchor_positive_dist.shape)
+    anchorpos_dist = tf.expand_dims(pairwise_dist, 2)
+    assert anchorpos_dist.shape[2] == 1, "{}".format(anchorpos_dist.shape)
 
-    anchor_negative_dist = tf.expand_dims(pairwise_dist, 1)
-    assert anchor_negative_dist.shape[1] == 1, "{}".format(anchor_negative_dist.shape)
+    anchorneg_dist = tf.expand_dims(pairwise_dist, 1)
+    assert anchorneg_dist.shape[1] == 1, "{}".format(anchorneg_dist.shape)
 
-    triplet_loss = anchor_positive_dist - anchor_negative_dist + alpha
+    tripletloss = anchorpos_dist - anchorneg_dist + alpha
 
     mask = _tripletmask(labels)
 
-    triplet_loss = tf.multiply(mask, triplet_loss)
-    triplet_loss = tf.maximum(triplet_loss, 0.0)
+    tripletloss = tf.multiply(mask, tripletloss)
+    tripletloss = tf.maximum(tripletloss, 0.0)
 
-    valid_triplets = tf.cast(tf.greater(triplet_loss, 1e-16), tf.float32)
-    num_positive_triplets = tf.reduce_sum(valid_triplets)
-    triplet_loss = tf.reduce_sum(triplet_loss) / (num_positive_triplets + 1e-16)
+    valid_triplets = tf.cast(tf.greater(tripletloss, 1e-16), tf.float32)
+    npos_triplets = tf.reduce_sum(valid_triplets)
+    tripletloss = tf.reduce_sum(tripletloss) / (npos_triplets + 1e-16)
 
-    return triplet_loss
+    return tripletloss
 
 def batch_hard(embeddings, labels, alpha = 0.2, squared=False):
-    pairwise_dist = _pairwise_distances(embeddings, squared=squared)
+    pairwise_dist = _pairwise_dist(embeddings, squared=squared)
 
-    mask_anchor_positive = _anchorpositivemask(labels)
-    anchor_positive_dist = tf.multiply(mask_anchor_positive, pairwise_dist)
-    hardest_positive_dist = tf.reduce_max(anchor_positive_dist, axis=1, keepdims=True)
-    tf.summary.scalar("hardest_positive_dist", tf.reduce_mean(hardest_positive_dist))
+    mask_anchorpos = _anchorpos_mask(labels)
+    anchorpos_dist = tf.multiply(mask_anchorpos, pairwise_dist)
+    hardest_pos_dist = tf.reduce_max(anchorpos_dist, axis=1, keepdims=True)
+    tf.summary.scalar("hardest_pos_dist", tf.reduce_mean(hardest_pos_dist))
 
-    mask_anchor_negative = _anchornegativemask(labels)
-    max_anchor_negative_dist = tf.reduce_max(pairwise_dist, axis=1, keepdims=True)
-    anchor_negative_dist = pairwise_dist + max_anchor_negative_dist * (1.0 - mask_anchor_negative)
-    hardest_negative_dist = tf.reduce_min(anchor_negative_dist, axis=1, keepdims=True)
-    tf.summary.scalar("hardest_negative_dist", tf.reduce_mean(hardest_negative_dist))
+    mask_anchorneg = _anchorneg_mask(labels)
+    max_anchorneg_dist = tf.reduce_max(pairwise_dist, axis=1, keepdims=True)
+    anchorneg_dist = pairwise_dist + max_anchorneg_dist * (1.0 - mask_anchorneg)
+    hardest_neg_dist = tf.reduce_min(anchorneg_dist, axis=1, keepdims=True)
+    tf.summary.scalar("hardest_neg_dist", tf.reduce_mean(hardest_neg_dist))
 
-    triplet_loss = tf.maximum(hardest_positive_dist - hardest_negative_dist + alpha, 0.0)
-    triplet_loss = tf.reduce_mean(triplet_loss)
+    tripletloss = tf.maximum(hardest_pos_dist - hardest_neg_dist + alpha, 0.0)
+    tripletloss = tf.reduce_mean(tripletloss)
 
-    return triplet_loss
+    return tripletloss
